5bc2406f49899b75c6c6dea7a28249cb078b2858
Himanshu sahu <hsahu8268@gmail.com>
2025-01-05 11:27:45 +0530

    add backend deployed url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,9 +1,9 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+// export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
+// export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
+// export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
+// export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
 
-// export const USER_API_END_POINT="https://jobhunt-9pyl.onrender.com/api/v1/user";
-// export const JOB_API_END_POINT="https://jobhunt-9pyl.onrender.com/api/v1/job";
-// export const APPLICATION_API_END_POINT="https://jobhunt-9pyl.onrender.com/api/v1/application";
-// export const COMPANY_API_END_POINT="https://jobhunt-9pyl.onrender.com/api/v1/company";
+export const USER_API_END_POINT="https://jobhunt-vr1n.onrender.com//api/v1/user";
+export const JOB_API_END_POINT="https://jobhunt-vr1n.onrender.com//api/v1/job";
+export const APPLICATION_API_END_POINT="https://jobhunt-vr1n.onrender.com//api/v1/application";
+export const COMPANY_API_END_POINT="https://jobhunt-vr1n.onrender.com//v1/company";
