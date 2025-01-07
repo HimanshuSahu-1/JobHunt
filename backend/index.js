ade91a9437a33bf7235fbc0a66f308c067ac2645
Himanshu sahu <hsahu8268@gmail.com>
2025-01-07 22:24:42 +0530

    Modilied some files

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:"https://jobhunt-38s3.onrender.com",
+    origin:"http://localhost:5173",
     credentials:true
 }
 
